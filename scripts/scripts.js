8bff10e8950fd9414d030ea8ea9bdf4531e808f0
odilamuni <odilamuni@gmail.com>
2019-04-25 20:07:41 -0400

    added portfolio mosaic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,9 @@
-
-            
+// external.js
+console.log("iniciando");
+var doc, bod, htm;
+addEventListener('load', function(){
+  doc = document; bod = doc.body; htm = doc.documentElement;
+  addEventListener('scroll', function(){
+    doc.querySelector('nav').style.backgroundColor = htm.scrollTop > 199 ? 'rgba(23, 155, 155,0.9)' : 'transparent';
+  });
+});
